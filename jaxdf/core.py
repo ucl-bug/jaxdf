86944e1b34d08d3443991fb8b069323a0df3570c
Antonio Stanziola <stanziola.antonio@gmail.com>
2022-05-27 16:03:22 +0100

    BREAKING CHANGE: changed how operator parameters are returned

--- jaxdf/core.py
+++ jaxdf/core.py
@@ -1,9 +1,12 @@
 
+import types
 from functools import wraps
 from typing import Callable
 
+from jax import jit
 from jax.tree_util import register_pytree_node_class, tree_map
 from plum import Dispatcher
+from plum.function import promised_type_of
 
 # Initialize the dispatch table
 _jaxdf_dispatch = Dispatcher()
@@ -13,26 +16,56 @@ debug_config ={
   "debug_dispatch": False,
 }
 
-def _operator(evaluate, precedence):
+def _operator(evaluate, precedence, init_params):
   @wraps(evaluate)
   def wrapper(*args, **kwargs):
+    if debug_config["debug_dispatch"]:
+      print(f"Dispatching {evaluate.__name__} with for types {evaluate.__annotations__}")
+
     outs = evaluate(*args, **kwargs)
     if isinstance(outs, tuple) and len(outs) > 1:
       # Overload the field class with an extra attribute
       field = outs[0]
-      field._op_params = outs[1]
     else:
       field = outs
 
-    if debug_config["debug_dispatch"]:
-      print(f"Dispatching {evaluate.__name__} with for types {evaluate.__annotations__}")
-
     return field
 
+  if init_params is None:
+    def init_params(*args, **kwargs):
+      @jit
+      def _only_params(*args, **kwargs):
+        try:
+          return evaluate(*args, **kwargs)[1]
+        except:
+          return None
+      return _only_params(*args, **kwargs)
+
+  wrapper._initialize_parameters = init_params
+
   f = _jaxdf_dispatch(wrapper, precedence=precedence)
+
+  # Bind an init_params method that returns the default parameters
+  def _bound_init_params(self, *args, **kwargs):
+    self._resolve_pending_registrations()
+    if not self._runtime_type_of:
+        sig_types = tuple([type(x) for x in args])
+    else:
+        sig_types = tuple([promised_type_of.resolve()(x) for x in args])
+
+    method, _ = self.resolve_method(*sig_types)
+    return method._initialize_parameters(*args, **kwargs)
+
+  f.default_params = types.MethodType(_bound_init_params, f)
+
   return f
 
-def operator(evaluate: Callable = None, precedence: int = 0):
+
+def operator(
+  evaluate: Callable = None,
+  init_params: Callable = None,
+  precedence: int = 0
+):
   r'''Decorator for defining operators using multiple dispatch. The type annotation of the
   `evaluate` function are used to determine the dispatch rules.
 
@@ -42,6 +75,8 @@ def operator(evaluate: Callable = None, precedence: int = 0):
     evaluate (Callable): A function with the signature `evaluate(field, *args, **kwargs, params)`.
       It must return a tuple, with the first element being a field and the second
       element being the default parameters for the operator.
+    init_params (Callable): A function that overrides the default parameters initializer for the
+      operator. Useful when running the operator just to get the parameters is expensive.
     precedence (int): The precedence of the operator if an ambiguous match is found.
 
   Returns:
@@ -67,10 +102,10 @@ def operator(evaluate: Callable = None, precedence: int = 0):
   if evaluate is None:
     # Returns the decorator
     def decorator(evaluate):
-      return _operator(evaluate, precedence)
+      return _operator(evaluate, precedence, init_params)
     return decorator
   else:
-    return _operator(evaluate, precedence)
+    return _operator(evaluate, precedence, init_params)
 
 
 def new_discretization(cls):
